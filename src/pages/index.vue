4203a66e88cf71816808eb6b37aabbce5fca632c
claire <qiuxiaocen@zhizukj.com>
2022-06-30 19:59:19 +0800

    feat: delete fotter

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,7 +13,10 @@ const go = (path) => {
     border-b="1 gray-600/30"
     hover:border-red-500
   >
-    <div font-mono text-xl hover:opacity-100 hover:text-red-500>
+    <div flex="~ gap2" font-mono text-xl hover:opacity-100 hover:text-red-500>
+      <button icon-btn @click="toggleDark()">
+        <div dark:i-carbon-moon i-carbon-sun />
+      </button>
       i-qxc
     </div>
     <div flex="~ gap5" justify-end>
