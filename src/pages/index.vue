52e583573f12bac35c4800a478e3e231b78cd5a6
qiuxiaocen <qiuxiaocen@kedacom.com>
2022-07-04 00:46:19 +0800

    feat: change home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,7 +26,71 @@ const go = (path: string) => {
       @card-clicked="go(card.url)"
     />
   </div>
-  <div p10 font-mono>
-    Much More fun is Building...
+  <div p10 font-mono class="a-inspector-loader hover:color-red-600">
+    Much More fun is Building
+    <span class="dots"><span>.</span><span>.</span><span>.</span></span>
   </div>
 </template>
+
+<style scoped>
+@keyframes dots-1 {
+  from {
+    opacity: 0
+  }
+  25% {
+    opacity: 1
+  }
+}
+@keyframes dots-2 {
+  from {
+    opacity: 0
+  }
+  50% {
+    opacity: 1
+  }
+}
+@keyframes dots-3 {
+  from {
+    opacity: 0
+  }
+  75% {
+    opacity: 1
+  }
+}
+@-webkit-keyframes dots-1 {
+  from {
+    opacity: 0
+  }
+  25% {
+    opacity: 1
+  }
+}
+@-webkit-keyframes dots-2 {
+  from {
+    opacity: 0
+  }
+  50% {
+    opacity: 1
+  }
+}
+@-webkit-keyframes dots-3 {
+  from {
+    opacity: 0
+  }
+  75% {
+    opacity: 1
+  }
+}
+.a-inspector-loader .dots span {
+  animation: dots-1 2s infinite steps(1);
+  -webkit-animation: dots-1 2s infinite steps(1);
+}
+.a-inspector-loader .dots span:first-child+span {
+  animation-name: dots-2;
+  -webkit-animation-name: dots-2;
+}
+.a-inspector-loader .dots span:first-child+span+span {
+  animation-name: dots-3;
+  -webkit-animation-name: dots-3;
+}
+</style>
